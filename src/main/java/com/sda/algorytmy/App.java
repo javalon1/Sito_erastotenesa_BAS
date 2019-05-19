8cc3d576ae8b816b5f8495ff99c8583167fb9e68
bart7801 <bart7801@o2.pl>
2019-05-19 09:21:04 +0100

    z wyswietleniem wyniku w tablicy

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -6,23 +6,27 @@ package com.sda.algorytmy;
  */
 public class App {
     public static void main(String[] args) {
+        {
+            Erastotenes.sito(30);
 
-        int k = 2;
-        while (k < 200) {
-            int licznik = 0;
+/*               int k = 2;
+            while (k < 200) {
+                int licznik = 0;
 
-            for (int i = 2; i < 2; i++) {
-                if (k % i == 0) {
-                    //System.out.println("Jest dzielnik: " + i);
-                    licznik++;
+                for (int i = 2; i < 2; i++) {
+                    if (k % i == 0) {
+                        //System.out.println("Jest dzielnik: " + i);
+                        licznik++;
+                    }
                 }
+                if (licznik == 0) {
+                    System.out.println(k + " jest liczba pierwsza");
+                } else {
+                    //System.out.println(k + " nie jest liczba pierwsza");
+                }
+                k++;
             }
-            if (licznik == 0) {
-                System.out.println(k + " jest liczba pierwsza");
-            } else {
-                //System.out.println(k + " nie jest liczba pierwsza");
-            }
-            k++;
+*/
         }
     }
 }
--- src/main/java/com/sda/algorytmy/Erastotenes.java
+++ src/main/java/com/sda/algorytmy/Erastotenes.java
@@ -1,14 +1,73 @@
 // 0 - false
 // 1 - true
 package com.sda.algorytmy;
-
+import java.util.Arrays;
 public class Erastotenes {
 
-    public static int[] sito(int n){
+    public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(sito(30)));
 
+        int[] tab = sito(20);
 
+        for (int i=0; i<tab.length; i++){
 
-        return null;
+            System.out.println(tab[i]);
+        }
     }
+
+    public static int[] sito(int n) {
+
+        int[] T = new int[n+1];
+        // defaultowo jet wypelniona zerami
+        // gdy cos wykreslamy to 1.
+
+        int i = 2;
+        int w = 0;
+        while (!(i >= n)) {
+
+            w = i + i;
+            while (!(w > n)) {
+
+                T[w] = 1;
+
+                w = w + i;
+
+            }
+
+            i = i + 1;
+
+        }
+        //wilekosc nowej tablicy
+
+        int count =0;
+
+        for (int j = 2; j <= n ; j++) {
+
+            if (T[j] == 0){
+
+                count++;
+
+            }
+
+        }
+        //nowa tablica
+
+        int[] tabwynik = new int[count];
+
+        int z =0;
+
+        for (int j = 2; j <= n ; j++) {
+
+            if (T[j] == 0){
+
+                tabwynik[z++]= j;
+
+            }
+
+        }
+        return tabwynik;
+
+    }
+
 }
